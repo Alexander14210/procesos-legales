987f5a460fe12ce05df2d0c0c6dc3b6c528fadb6
Alejandro Santamaria <santamariaalejandro232@gmail.com>
2023-09-13 23:31:25 -0500

    Add test form

--- api/api.py
+++ api/api.py
@@ -1,8 +1,20 @@
 import time
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/email', methods=["POST"])
+def handle_email():
+    data = request.get_json()
+    print(data)
+    return 'Enviar email a ' + data['abogado']
+
+
+@app.route('/supervisor')
+def hello_supervisor():
+    return {'name': "SUPERVISOR"}
--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-
+function Time() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
@@ -12,23 +11,64 @@ function App() {
     });
   }, []);
 
+  return <p>The current time is {currentTime}.</p>;
+}
 
+function AsignarDemanda(){
+  const [abogado, setAbogado] = useState("ABOGADO_A");
+
+  const handleChange = (event) => {
+    setAbogado(event.target.value);
+    
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(abogado);
+    fetch('/email', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({abogado: abogado})
+    });
+  }
+ 
+  return (
+    <div>
+    <p>Seleccione un abogado para asignar la demanda</p>
+    <form onSubmit={handleSubmit}>
+      <select value={abogado} onChange={handleChange}>
+        <option value="ABOGADO_A">ABOGADO A</option>
+        <option value="ABOGADO_B">ABOGADO B</option>
+      </select>
+      <input type="submit" />
+    </form>
+    </div>
+    
+  )
+}
+
+
+function Header() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetch('/supervisor').then(res => res.json()).then(data => {
+      setName(data.name);
+    });
+  },[]);
+
+
+  return <p>Bienvenido, {name}.</p>;
+}
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
+       <AsignarDemanda></AsignarDemanda>
       </header>
     </div>
   );
